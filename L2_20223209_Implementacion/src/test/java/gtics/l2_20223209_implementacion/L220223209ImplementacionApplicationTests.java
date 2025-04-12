5c3217f3ef99bac7e04c89ff06ee8e67dc2d8b36
Tony F. <a20223209@pucp.edu.pe>
2025-04-11 20:30:25 -0500

    Creacion del proyecto Springboot

---
+++ L2_20223209_Implementacion/src/test/java/gtics/l2_20223209_implementacion/L220223209ImplementacionApplicationTests.java
@@ -0,0 +1,13 @@
+package gtics.l2_20223209_implementacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L220223209ImplementacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

59b0082d144bec54eb06675f057f5db79a373152
Tony F. <a20223209@pucp.edu.pe>
2025-04-11 20:41:02 -0500

    Creacion de entidades + Application Properties

---
+++ L2_20223209_Implementacion/src/main/java/gtics/l2_20223209_implementacion/Entity/Auto.java
@@ -0,0 +1,26 @@
+package gtics.l2_20223209_implementacion.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+
+@Table(name="auto")
+public class Auto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idauto", nullable = false)
+    private Integer idAuto;
+    @Column(name = "modelo", nullable = false, length = 45)
+    private String modelo;
+    @Column(name = "color", nullable = false, length = 45)
+    private String color;
+    @Column(name = "kilometraje", nullable = false)
+    private Integer kilometraje;
+    @Column(name = "costo_por_dia", nullable = false)
+    private float costoPorDia;
+    @Column(name = "sede_idsede", nullable = false)
+    private Integer sedeIdSede;
+}
